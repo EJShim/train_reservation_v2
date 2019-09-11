545b7fc0a28014663b832f53d2ef4b75a8e69ea1
EJShim <soulrommel@gmail.com>
2019-09-11 17:45:25 +0900

    udpate

--- content.js
+++ content.js
@@ -1,12 +1,22 @@
 // content.js
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+//var firstHref = $("a[href^='http']").eq(0).attr("href");
 
+//Lets see what I can do
+document.title = "automated korail"
+var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = chrome.extension.getURL('favicon.ico');
+document.getElementsByTagName('head')[0].appendChild(link);
 
+document.onmousedown = e=>{
+    e.preventDefault();
+    console.log("asdfasdf");
+}
 // content.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
       if( request.message === "clicked_browser_action" ) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
-  
-        console.log(firstHref);
+        
+        console.log(document.onmousedown);
       }
 });
