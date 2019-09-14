de84796dd97b455aba6d5277d4d3687751aad992
Eungjune Shim <eungjuneshim@Eungjuneui-MacBook-Pro.local>
2019-09-14 15:37:35 +0900

    update

--- utils.js
+++ utils.js
@@ -18,7 +18,7 @@ function MakeCheckbox(){
     const input = document.createElement('input')
     input.setAttribute("type", "checkbox");  
     label.appendChild(input);
-    label.appendChild(document.createTextNode("mcr"));     
+    label.appendChild(document.createTextNode("매크로"));     
 
     return label;
 }
